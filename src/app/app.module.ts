5932c5cfbb3e8c1828881d2463ffe8b11b62cc84
Arturo Pardo <arturopardosanchez@gmail.com>
2019-03-20 08:29:31 +0100

    components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import { ShowListsComponent } from './show-lists/show-lists.component';
+import { RegisterViewComponent } from './register-view/register-view.component';
+import { ListComponent } from './list/list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BoardComponent } from './board/board.component';
+import { LoginViewComponent } from './login-view/login-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    ShowListsComponent,
+    RegisterViewComponent,
+    ListComponent,
+    NavbarComponent,
+    BoardComponent,
+    LoginViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/show-lists/show-lists.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-lists',
+  templateUrl: './show-lists.component.html',
+  styleUrls: ['./show-lists.component.sass']
+})
+export class ShowListsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
